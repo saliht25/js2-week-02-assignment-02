cfd9d4a7bb3bd5a52b4a93f826613ed52ae8e774
saliht25 <107279476+saliht25@users.noreply.github.com>
2022-11-24 20:28:43 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,5 +1,27 @@
-const changeEmailFromGmailToHicoders = () => {};
+const changeEmailFromGmailToHicoders = (pArr) => {
+    let change_email=pArr
+    .filter((element)=>element.includes(41))
+    .sort()
+    .map((email)=>email.toLowerCase().replace(["gmail.com"],["hicoders.ch"]));
+    return change_email;
 
-const findSuperNumbers = () => {};
+
+};
+
+
+const findSuperNumbers = (pArr) => {
+    let superNumArr = [];
+    pArr.map(superNumber=>{ 
+      let numberList = String(superNumber).split("").map(Number) 
+      let totalNumber=0;
+      numberList.map(element=>{  
+          totalNumber+=Math.pow(element,numberList.length) 
+      })
+      if(totalNumber===superNumber){
+          superNumArr.push(superNumber)
+      }
+    })
+    return superNumArr;
+};
 
 export { changeEmailFromGmailToHicoders, findSuperNumbers };
